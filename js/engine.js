773ae8b09ac4bf08a54af2b0240269072e2871c0
Yogesh Manni <yogeshmanni786@gmail.com>
2018-02-11 16:02:52 +0530

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -178,7 +178,6 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png',
         'images/Star.png',
         'images/char-horn-girl.png'
 
